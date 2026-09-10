a432ce2239f9ea67b4c3f2af97e5b04edc809493
IsaacCM123 <condoriisaac06@gmail.com>
2026-09-10 17:49:23 -0400

    Version F

--- admin/core/1_conexion.php
+++ admin/core/1_conexion.php
@@ -6,11 +6,10 @@ $pass = getenv('DB_PASS');
 $db   = getenv('DB_NAME');
 $ssl  = __DIR__ . '/../../BD/cert/globalsignrootca.pem';
 
-$conexion = mysqli_init()
+$conexion = mysqli_init();
 
-mysqli_ssl_set($conexion,NULL,NULL,$ssl,NULL,NULL)
+mysqli_ssl_set($conexion,NULL,NULL,$ssl,NULL,NULL);
 
-$exito = mysqli_real_connect($conexion,$host,$user,$pass,$db,(int)$port,NULL,MYSQLI_CLIENT_SSL)
-if ($exito) { die('Conexion Exitosa') } else{ die('Error en conexion') }
+$exito = mysqli_real_connect($conexion,$host,$user,$pass,$db,(int)$port,NULL,MYSQLI_CLIENT_SSL);
+if ($exito) { die('Conexion Exitosa'); } else{ die('Error en conexion'); }
 // El archivo que haga "require" será responsable de cerrarla con mysqli_close($conexion) al terminar.
-?>
