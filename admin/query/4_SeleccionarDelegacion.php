07e9c517a543c9c613184e615e65772610aafa86
IsaacCM123 <condoriisaac06@gmail.com>
2026-09-13 20:53:52 -0400

    Version V

---
+++ admin/query/4_SeleccionarDelegacion.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '/../core/1_conexion.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+    <h1>Este es el formulario 4_SeleccionarDelegacion.php</h1>
+</body>
+</html>
