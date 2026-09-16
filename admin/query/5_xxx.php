884fd13aeeae64b4d9c150973454d0dc42b473b6
IsaacCM123 <condoriisaac06@gmail.com>
2026-09-16 19:50:50 -0400

    Version III

--- admin/query/5_xxx.php
+++ admin/query/5_xxx.php
@@ -5,19 +5,24 @@ $dni = $_POST['dniPOSTParaPHP'];
 $nombre = $_POST['nombrePOSTParaPHP'];
 
 $Seleccionar = "SELECT      p.categoria,
+                            p.extra,
+                            p.telefono,
                             i.nombreIglesia,
                             i.ciudad,
-                            e.nombreEsposa,
-                            per.nombrePersona AS nombreAnfitrion,
-                            m.nombreMesa
+                            i.pais,
+                            m.nombreMesa,
+                            per.nombrePersona,
+                            a.celular,
+                            e.nombreEsposa
+                            
                 FROM        tbl_pastor p
-                LEFT JOIN   tbl_ministerio mi ON mi.FK_dniPastor = p.dniPastor
-                LEFT JOIN   tbl_iglesia i     ON i.idIglesia = mi.PKFK_idIglesia
-                LEFT JOIN   tbl_esposa e      ON e.FK_dniPastor = p.dniPastor
-                LEFT JOIN   tbl_hospedaje h   ON h.FK_dniPastor = p.dniPastor
-                LEFT JOIN   tbl_anfitrion a   ON a.PKFK_idPersona = h.FK_idAnfitrion
-                LEFT JOIN   tbl_persona per   ON per.idPersona = a.PKFK_idPersona
-                LEFT JOIN   tbl_mesa m        ON m.idMesa = p.FK_idMesa
+                LEFT JOIN   tbl_ministerio mi ON mi.FK_dniPastor    = p.dniPastor
+                LEFT JOIN   tbl_iglesia i     ON i.idIglesia        = mi.PKFK_idIglesia
+                LEFT JOIN   tbl_esposa e      ON e.FK_dniPastor     = p.dniPastor
+                LEFT JOIN   tbl_hospedaje h   ON h.FK_dniPastor     = p.dniPastor
+                LEFT JOIN   tbl_anfitrion a   ON a.PKFK_idPersona   = h.FK_idAnfitrion
+                LEFT JOIN   tbl_persona per   ON per.idPersona      = a.PKFK_idPersona
+                LEFT JOIN   tbl_mesa m        ON m.idMesa           = p.FK_idMesa
                 WHERE       p.dniPastor = '$dni'";
 
 $resultado = mysqli_query($conexion, $Seleccionar);
@@ -28,11 +33,15 @@ if($resultado){
             'dni'           => $dni,
             'nombre'        => $nombre,
             'categoria'     => $fila['categoria'],
+            'extra'         => $fila['extra'],
+            'telefono'      => $fila['telefono'],
             'iglesia'       => $fila['nombreIglesia'],
             'ciudad'        => $fila['ciudad'],
-            'esposa'        => $fila['nombreEsposa'],
-            'anfitrion'     => $fila['nombreAnfitrion'],
-            'mesa'          => $fila['nombreMesa']
+            'pais'          => $fila['pais'],
+            'mesa'          => $fila['nombreMesa'],
+            'anfitrion'     => $fila['nombrePersona'],
+            'celular'     => $fila['celular'],
+            'esposa'        => $fila['nombreEsposa']
         ];
     }
 }
