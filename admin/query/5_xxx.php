74f287d12740e4c1e94505f114e09a4cc30b0a4f
IsaacCM123 <condoriisaac06@gmail.com>
2026-09-16 19:05:56 -0400

    Version XHI

--- admin/query/5_xxx.php
+++ admin/query/5_xxx.php
@@ -4,19 +4,35 @@ require __DIR__ . '/../core/1_conexion.php';
 $dni = $_POST['dniPOSTParaPHP'];
 $nombre = $_POST['nombrePOSTParaPHP'];
 
-$Seleccionar = "SELECT categoria,extra,telefono,FK_idMesa from tbl_pastor where dniPastor = '$dni'";
+$Seleccionar = "SELECT      p.categoria,
+                            i.nombreIglesia,
+                            i.ciudad,
+                            e.nombreEsposa,
+                            per.nombrePersona AS nombreAnfitrion,
+                            m.nombreMesa
+                FROM        tbl_pastor p
+                LEFT JOIN   tbl_ministerio mi ON mi.FK_dniPastor = p.dniPastor
+                LEFT JOIN   tbl_iglesia i     ON i.idIglesia = mi.PKFK_idIglesia
+                LEFT JOIN   tbl_esposa e      ON e.FK_dniPastor = p.dniPastor
+                LEFT JOIN   tbl_hospedaje h   ON h.FK_dniPastor = p.dniPastor
+                LEFT JOIN   tbl_anfitrion a   ON a.PKFK_idPersona = h.FK_idAnfitrion
+                LEFT JOIN   tbl_persona per   ON per.idPersona = a.PKFK_idPersona
+                LEFT JOIN   tbl_mesa m        ON m.idMesa = p.FK_idMesa
+                WHERE       p.dniPastor = '$dni'";
 
 $resultado = mysqli_query($conexion, $Seleccionar);
 $datos = [];
 if($resultado){
     if($fila = mysqli_fetch_assoc($resultado)) {
         $datos = [
-            'dni'        => $dni,
-            'nombre'     => $nombre,
-            'categoria'  => $fila['categoria'],
-            'extra'      => $fila['extra'],
-            'telefono'   => $fila['telefono'],
-            'idMesa'     => $fila['FK_idMesa']
+            'dni'           => $dni,
+            'nombre'        => $nombre,
+            'categoria'     => $fila['categoria'],
+            'iglesia'       => $fila['nombreIglesia'],
+            'ciudad'        => $fila['ciudad'],
+            'esposa'        => $fila['nombreEsposa'],
+            'anfitrion'     => $fila['nombreAnfitrion'],
+            'mesa'          => $fila['nombreMesa']
         ];
     }
 }
